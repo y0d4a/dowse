96100acb37be143a10a09357f619c59d9722d0e9
Nicola Rossi <nicola@dyne.org>
2017-05-18 12:55:52 +0200

    WIP

--- src/webui/src/captive_portal_admin.c
+++ src/webui/src/captive_portal_admin.c
@@ -21,6 +21,21 @@
  */
 #include <webui.h>
 
+
+/**
+ * @file captive_portal_admin.c
+ * @author Nicola Rossi <nicola@dyne.org>
+ * @date Stardate -305623.39963850833
+ * @brief This function serve the captive_portal_admin page request.
+ *
+ *
+ * In this function will be rendered the captive_portal_admin page request, providing:
+ *  - aggregate state of all things in the Dowse network or rather all things are on or off
+ *  - event grouped by mac address and typology
+ *  - Temporarily, changing the redirection to captive_portal_admin disabling it, until the next event will be triggered.
+ *
+ */
+
 int captive_portal_admin(struct http_request * req) {
     log_entering();
     template_t tmpl;
@@ -31,12 +46,13 @@ int captive_portal_admin(struct http_request * req) {
     out = kore_buf_alloc(0);
 	attr=attrinit();
 
+    /* aggregate state of all things */
     sprintf(line,"select value as cur_state from parameter where variable='state all things'");
     sql_select_into_attributes(line,NULL,&attr);
 
     load_current_identity(req,&attr);
-	/**/
 
+    /* event grouped by mac address and tipology */
     sql_select_into_attributes(
             "SELECT count(id) as how_much, min(age) as first_age,max(age) as last_age,E.* "
             "FROM event E WHERE recognized=0 "
@@ -44,13 +60,14 @@ int captive_portal_admin(struct http_request * req) {
             "captive_portal_event",
             &attr);
 
-    /* Put the captive portal redirect */
+    /* Change the redirection to captive_portal_admin disabling it, until the next event will be triggered. */
     sql_execute("CALL redirect_admin_to_captive_portal('admin');",&attr);
 
+    /* Render the page */
     template_load("assets/captive_portal_admin.html",&tmpl);
     template_apply(&tmpl,attr,out);
 
-	/**/
+	/* Free resource */
     html_rendered = kore_buf_release(out, &len);
     http_response(req, 200, html_rendered, len);
 
