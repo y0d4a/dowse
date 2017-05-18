f6981ad6e1478a22dc5f08a0f79173c97ebcb6c3
Nicola Rossi <nicola@dyne.org>
2017-05-18 11:25:56 +0200

    Rename

--- src/webui/src/admin_should_handle_new_event.c
+++ src/webui/src/admin_should_handle_new_event.c
@@ -19,9 +19,30 @@
  *
  */
 
+
+
+/**
+ * @file admin_should_handle_new_event.c
+ * @author Nicola Rossi <nicola@dyne.org>
+ * @date Stardate -305623.39963850833
+ * @brief This file contain the function related to admin captive portal redirect .
+ *
+ */
+
 #include <webui.h>
 
-int admin_should_handle_event(char*macaddr) {
+
+
+/**
+ * @brief Function to evaluate if administrator should handle Dowse event
+ *
+ * Function to evaluate if administrator should handle Dowse event.
+ * It checks on Redis DB if is present the key authorization-mac-<CONNECTING-MAC-ADDRESS>
+ * and return 1 it's value is equal to the constant __R_ADMIN_SHOULD_CHECK = "admin_should_check"
+ *
+ */
+
+int admin_should_handle_new_event(char*macaddr) {
     char key[256];
     char value[256];
     sprintf(key,"authorization-mac-%s",to_upper(macaddr));
@@ -31,6 +52,10 @@ int admin_should_handle_event(char*macaddr) {
 
 }
 
+/**
+ *  @brief Function to get on Redis the value of a key
+ *
+ * */
 int redis_get_key(const char* key, char*value, int sizeof_value) {
     redisContext *redis = NULL;
     redisReply *reply = NULL;
@@ -58,6 +83,19 @@ int redis_get_key(const char* key, char*value, int sizeof_value) {
 
 }
 
+/**
+ * @brief This function check if there are event that the admin should recognize.
+ *
+ * This function is used in the management of dowse-events.
+ * It's different by admin_should_handle_new_event because it check on the DB,
+ *  if there are event to recognize or not.
+ *
+ * eg: an event is triggered in the dowse ecosystem,
+ *   then the admin should be redirected on the dowse captive_portal_admin page.
+ *   the admin evaluate him navigation more important that the recognition of the event so leaves it in stand-by .
+ *   The admin_should_handle_new_event will return "0" but "there_are_event_not_recognized" should return "1"
+ *
+ */
 int there_are_event_not_recognized() {
     char sql[] =
             "SELECT * FROM event WHERE not recognized and description='new_mac_address'";
@@ -65,10 +103,12 @@ int there_are_event_not_recognized() {
     dummy_attribute=attrinit();
     int rv = sql_select_into_attributes(sql, "dummy", &dummy_attribute);
     attrfree(dummy_attribute);
-    func(" admin_should_handle_event = %d", rv);
+    func(" admin_should_handle_new_event = %d", rv);
     if (rv > 0) {
         return 1;
     } else {
         return 0;
     }
 }
+
+
